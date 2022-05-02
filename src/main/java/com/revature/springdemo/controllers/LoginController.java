f6283a517484d51be276d1c231f447649d8dfc2e
cloudcare-stack <co8mcate@gmail.com>
2022-05-02 01:21:19 -0700

    passing value from get to post html form

--- src/main/java/com/revature/springdemo/controllers/LoginController.java
+++ src/main/java/com/revature/springdemo/controllers/LoginController.java
@@ -3,15 +3,21 @@ package com.revature.springdemo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login") //GET, POST
-    public String dummy(ModelMap model){
-        //model.put("name", name);
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage(ModelMap model){
         return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String showWelcomePage(@RequestParam String name, ModelMap model){
+        model.put("name", name);
+        return "welcome";
     }
 }
